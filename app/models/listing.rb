7283f5fac403a7f69aefaf8d9e22543b125b1adb
NadimRai <rai_nadim@yahoo.com>
2017-05-22 00:38:31 -0700

    added user id to listings and created association between user and listings

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,4 +1,10 @@
 class Listing < ApplicationRecord
 	has_attached_file :image, styles: { medium: "800x800>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   	validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+
+  	validates :name, :description, :price, presence: true
+  	validates :price, numericality: { greater_than: 0 }
+  	validates_attachment_presence :image
+
+  	belongs_to :user
 end
