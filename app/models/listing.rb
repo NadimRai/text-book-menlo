bf4084992925b98dc834581aac128333190ede1b
NadimRai <rai_nadim@yahoo.com>
2017-05-21 22:32:01 -0700

    styled listing show page

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,4 +1,4 @@
 class Listing < ApplicationRecord
-	has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+	has_attached_file :image, styles: { medium: "800x800>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   	validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
