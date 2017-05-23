19612a0b2560138bd36ff2e9bb1162dea503d360
NadimRai <rai_nadim@yahoo.com>
2017-05-23 01:32:51 -0700

    ability to place order with seller buyer id

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  resources :orders
+  
   devise_for :users
-  resources :listings
+
+  resources :listings do
+    resources :orders, only: [:new, :create]
+  end
+  
   get 'pages/about'
 
   get 'pages/contact'
