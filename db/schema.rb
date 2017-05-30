63a298151a93d25cc47e5ed8de56d4e1dfe62198
NadimRai <rai_nadim@yahoo.com>
2017-05-30 13:20:32 -0700

    generated model review

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170523075621) do
+ActiveRecord::Schema.define(version: 20170530201818) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,14 @@ ActiveRecord::Schema.define(version: 20170523075621) do
     t.integer "listing_id"
     t.integer "buyer_id"
     t.integer "seller_id"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string "rating"
+    t.string "integer"
+    t.text "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
