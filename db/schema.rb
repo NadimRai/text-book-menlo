51c659ae44e3e92ea4ca8d1d97f84f7f94c94748
NadimRai <rai_nadim@yahoo.com>
2017-05-30 13:24:55 -0700

    add user and listing id to review

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170530201818) do
+ActiveRecord::Schema.define(version: 20170530202323) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,8 @@ ActiveRecord::Schema.define(version: 20170530201818) do
     t.text "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.integer "listing_id"
   end
 
   create_table "users", force: :cascade do |t|
