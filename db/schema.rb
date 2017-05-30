31a3303e280e5367f6aae724687ef8df7f5fb0f8
NadimRai <rai_nadim@yahoo.com>
2017-05-30 14:45:46 -0700

    ability to add reviews

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170530202323) do
+ActiveRecord::Schema.define(version: 20170530210016) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,13 +40,12 @@ ActiveRecord::Schema.define(version: 20170530202323) do
   end
 
   create_table "reviews", force: :cascade do |t|
-    t.string "rating"
-    t.string "integer"
     t.text "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
     t.integer "listing_id"
+    t.integer "rating"
   end
 
   create_table "users", force: :cascade do |t|
