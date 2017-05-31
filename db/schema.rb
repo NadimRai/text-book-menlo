1e259e89c0fc7e56992ad582728f634a667fb150
NadimRai <rai_nadim@yahoo.com>
2017-05-30 17:36:16 -0700

    added category_id to listing

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170531003214) do
+ActiveRecord::Schema.define(version: 20170531003511) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20170531003214) do
     t.integer "image_file_size"
     t.datetime "image_updated_at"
     t.integer "user_id"
+    t.integer "category_id"
   end
 
   create_table "orders", force: :cascade do |t|
