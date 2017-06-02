8bac6af0bce952d862f1e38d9aa52e248c55514b
NadimRai <rai_nadim@yahoo.com>
2017-06-02 01:35:18 -0700

    ability to rescue active record not found

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -98,6 +98,10 @@ class ListingsController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_listing
       @listing = Listing.find(params[:id])
+
+     rescue ActiveRecord::RecordNotFound
+      flash[:alert] = "The page you requested does not exist"
+      redirect_to root_url
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
