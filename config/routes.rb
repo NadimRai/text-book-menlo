ca45944617bf45a608cd6f34b08a2766ae0c44ba
NadimRai <rai_nadim@yahoo.com>
2017-06-12 16:01:03 -0700

    define route for omniauth

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   
-  
 
-  devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
   resources :listings do
     resources :orders, only: [:new, :create]
