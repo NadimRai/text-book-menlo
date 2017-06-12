31a3f3f76d8954b2b10ba241942ad5f7e0e7ec50
NadimRai <rai_nadim@yahoo.com>
2017-06-12 16:30:01 -0700

    added omniauth method to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,21 @@ class User < ApplicationRecord
    has_many :sales, class_name: "Order", foreign_key: "seller_id"
    has_many :purchases, class_name: "Order", foreign_key: "buyer_id"
    has_many :reviews
+
+   def self.new_with_session(params, session)
+  		super.tap do |user|
+    		if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+      			user.email = data["email"] if user.email.blank?
+    		end
+  		end
+	end
+
+	def self.from_omniauth(auth)
+	  where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+	    user.email = auth.info.email
+	    user.password = Devise.friendly_token[0,20]
+	    user.name = auth.info.name   # assuming the user model has a name
+	    
+	  end
+	end
 end
